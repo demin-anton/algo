52585e00e920f6350cb252d105fa3469d63705b1
Anton Demin <demin_anton@list.ru>
2016-12-10 16:31:32 +0300

    Bubble Sort Class

---
+++ sort/BubbleSort.java
@@ -0,0 +1,31 @@
+package ademin.algo.sort;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Anton on 10.12.2016.
+ */
+public class BubbleSort {
+    public static ArrayList<Integer> run(ArrayList<Integer> arrParam) {
+        long startTime = System.currentTimeMillis();
+        System.out.println("BubbleSort start...");
+        ////////////
+        ArrayList<Integer> arr = (ArrayList<Integer>) arrParam.clone();
+        int tmp;
+        for (int i = 0; i < arr.size() - 1; i++) {
+            for (int j = 0; j < arr.size() - 1 - i; j++) {
+                if (arr.get(j) > arr.get(j + 1)) {
+                    tmp = arr.get(j);
+                    arr.set(j, arr.get(j + 1));
+                    arr.set(j + 1, tmp);
+                }
+            }
+        }
+        ////////////
+        long endTime = System.currentTimeMillis();
+        System.out.println(arr);
+        System.out.println("BubbleSort end...");
+        System.out.println("BubbleSort work " + (endTime - startTime) + " miliseconds");
+        return arr;
+    }
+}
