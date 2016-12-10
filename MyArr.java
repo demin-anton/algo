1b6824b32b17e92f364627034121039df02ab9c5
Anton Demin <demin_anton@list.ru>
2016-12-10 20:28:24 +0300

    add overload function

--- MyArr.java
+++ MyArr.java
@@ -6,12 +6,15 @@ import java.util.ArrayList;
  * Created by Anton on 10.12.2016.
  */
 public class MyArr {
-    public static ArrayList<Integer> getArr(int n) {
+    public static ArrayList<Integer> getArr(int len) {
+        return getArr(len, 1000);
+    }
+
+    public static ArrayList<Integer> getArr(int len, int maxVal) {
         ArrayList<Integer> arr = new ArrayList<Integer>();
-        for (int i = 0; i < n; i++) {
-            arr.add((int) (Math.random() * 1000));
+        for (int i = 0; i < len; i++) {
+            arr.add((int) (Math.random() * maxVal));
         }
-        System.out.println(arr);
         return arr;
     }
 }
