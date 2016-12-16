e96744bd42cdc4292c9c263d82a45bf7732f600a
Demin Anton <demin_anton@list.ru>
2016-12-16 13:53:42 +0300

    update to int[]

--- sort/InsertionSort.java
+++ sort/InsertionSort.java
@@ -7,25 +7,28 @@ import java.util.ArrayList;
  * Created by ADemin on 12.12.2016.
  */
 public class InsertionSort {
-    public static ArrayList<Integer> run(ArrayList<Integer> arrParam) {
+    public static int[] run(int[] arrParam) {
         long startTime = System.currentTimeMillis();
         System.out.println("InsertionSort start...");
         ////////////
-        ArrayList<Integer> arr = (ArrayList<Integer>) arrParam.clone();
+        int[] arr = arrParam.clone();
+        int iter = 0;
         int tmp;
         int j;
-        for (int i = 1; i < arr.size(); i++) {
-            tmp = arr.get(i);
+        for (int i = 1; i < arr.length; i++) {
+            iter++;
+            tmp = arr[i];
             j = i;
-            while (j > 0 && arr.get(j - 1) > tmp) {
-                arr.set(j, arr.get(j - 1));
+            while (j > 0 && arr[j - 1] > tmp) {
+                iter++;
+                arr[j] = arr[j - 1];
                 j = j - 1;
             }
-            arr.set(j, tmp);
+            arr[j] = tmp;
         }
         ////////////
         long endTime = System.currentTimeMillis();
-        System.out.println("InsertionSort end... it work " + (endTime - startTime) + " miliseconds");
+        System.out.println("InsertionSort end... it work " + (endTime - startTime) + " miliseconds; " + iter + " iterations;");
         return arr;
     }
 }
