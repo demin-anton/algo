56aa25670a4863982d97b86c2b4a2c361a236a70
Demin Anton <demin_anton@list.ru>
2016-12-16 13:53:23 +0300

    update to int[]

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -7,24 +7,27 @@ import java.util.ArrayList;
  * Created by Anton on 10.12.2016.
  */
 public class BubbleSort {
-    public static ArrayList<Integer> run(ArrayList<Integer> arrParam) {
+    public static int[] run(int[] arrParam) {
         long startTime = System.currentTimeMillis();
         System.out.println("BubbleSort start...");
         ////////////
-        ArrayList<Integer> arr = (ArrayList<Integer>) arrParam.clone();
+        int iter = 0;
+        int[] arr = arrParam.clone();
         int tmp;
-        for (int i = 0; i < arr.size() - 1; i++) {
-            for (int j = 0; j < arr.size() - 1 - i; j++) {
-                if (arr.get(j) > arr.get(j + 1)) {
-                    tmp = arr.get(j);
-                    arr.set(j, arr.get(j + 1));
-                    arr.set(j + 1, tmp);
+        for (int i = 0; i < arr.length - 1; i++) {
+            iter++;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                iter++;
+                if (arr[j] > arr[j + 1]) {
+                    tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
                 }
             }
         }
         ////////////
         long endTime = System.currentTimeMillis();
-        System.out.println("BubbleSort end... it work " + (endTime - startTime) + " miliseconds");
+        System.out.println("BubbleSort end... it work " + (endTime - startTime) + " miliseconds; " + iter + " iterations;");
         return arr;
     }
 }
