cb48e23c6527c02f7d8fbaff89fc33a46c695cbe
Demin Anton <demin_anton@list.ru>
2016-12-16 13:53:55 +0300

    update to int[]

--- sort/SelectionSort.java
+++ sort/SelectionSort.java
@@ -7,29 +7,32 @@ import java.util.ArrayList;
  * Created by ADemin on 12.12.2016.
  */
 public class SelectionSort {
-    public static ArrayList<Integer> run(ArrayList<Integer> arrParam) {
+    public static int[] run(int[] arrParam) {
         long startTime = System.currentTimeMillis();
         System.out.println("SelectionSort start...");
         ////////////
-        ArrayList<Integer> arr = (ArrayList<Integer>) arrParam.clone();
+        int iter = 0;
+        int[] arr = arrParam.clone();
         int min = Integer.MAX_VALUE;
         int index = 0;
         int tmp = 0;
-        for (int j = 0; j < arr.size() - 1; j++) {
-            for (int i = j; i < arr.size(); i++) {
-                if (arr.get(i) < min) {
-                    min = arr.get(i);
+        for (int j = 0; j < arr.length - 1; j++) {
+            iter++;
+            for (int i = j; i < arr.length; i++) {
+                iter++;
+                if (arr[i] < min) {
+                    min = arr[i];
                     index = i;
                 }
             }
-            tmp = arr.get(j);
-            arr.set(j, min);
-            arr.set(index, tmp);
+            tmp = arr[j];
+            arr[j] = min;
+            arr[index] = tmp;
             min = Integer.MAX_VALUE;
         }
         ////////////
         long endTime = System.currentTimeMillis();
-        System.out.println("SelectionSort end... it work " + (endTime - startTime) + " miliseconds");
+        System.out.println("SelectionSort end... it work " + (endTime - startTime) + " miliseconds; " + iter + " iterations;");
         return arr;
     }
 }
