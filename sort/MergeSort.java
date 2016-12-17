4e73c136c6d4e2a7751a62f06403a0325fa95aaf
Anton Demin <demin_anton@list.ru>
2016-12-17 17:29:40 +0300

    end method

--- sort/MergeSort.java
+++ sort/MergeSort.java
@@ -1,23 +1,75 @@
 package ademin.algo.sort;
 
-import java.util.ArrayList;
+import ademin.algo.MyArr;
+
 import java.util.Arrays;
-import java.util.List;
+import java.util.Stack;
 
 /**
  * Сортировка слиянием
  * Created by Anton on 14.12.2016.
  */
 public class MergeSort {
+    public static long iter = 0;
+
     public static int[] run(int[] arrParam) {
         long startTime = System.currentTimeMillis();
         System.out.println("MergeSort start...");
         ////////////
-        int iter = 0;
-        int[] arr = arrParam.clone();
+        int[] arr = sorting(arrParam.clone());
         ////////////
         long endTime = System.currentTimeMillis();
         System.out.println("MergeSort end... it work " + (endTime - startTime) + " miliseconds; " + iter + " iterations;");
+        return arr;
+    }
+
+    public static int[] sorting(int[] arr) {
+        int len = arr.length;
+        if (len <= 1) {
+            return arr;
+        }
+        int mid = arr.length / 2;
+        int[] left = sorting(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = sorting(Arrays.copyOfRange(arr, mid, len));
+
+
+        return merge(left, right);
+
+    }
+
+    public static int[] merge(int[] l, int[] r) {
+        int[] arr = new int[l.length + r.length];
+        ////
+        Stack<Integer> left = new Stack<Integer>();
+        for (int k = l.length - 1; k >= 0; k--) {
+            left.push(l[k]);
+        }
+
+        Stack<Integer> right = new Stack<Integer>();
+        for (int k = r.length - 1; k >= 0; k--) {
+            right.push(r[k]);
+        }
+        int i = 0;
+        while (!left.empty() && !right.empty()) {
+            iter++;
+            if (left.peek() < right.peek()) {
+                arr[i] = left.pop();
+            } else {
+                arr[i] = right.pop();
+            }
+            i++;
+        }
+        while (!right.empty()) {
+            iter++;
+            arr[i] = right.pop();
+            i++;
+        }
+        while (!left.empty()) {
+            iter++;
+            arr[i] = left.pop();
+            i++;
+        }
+
         return arr;
     }
 }
